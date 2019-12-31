b4d499b323617f02f848c310cfc1109a8840466b
Noah Harris <jnharris@boomtownroi.com>
2019-12-30 22:32:52 -0500

    Add interface for writing and reading from mongo
    Add interface for creating and fetching media references from mongodb

--- storage/mongo.go
+++ storage/mongo.go
@@ -3,14 +3,17 @@ package storage
 import (
 	"context"
 	"fmt"
-	// "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"monke-cdn/util"
 	"time"
 )
 
 var client *mongo.Client
 var database *mongo.Database
+var media *mongo.Collection
 
 /*
  * seconds  -> context time
@@ -41,7 +44,62 @@ func ConnectTo(login, uri, name string) (err error) {
 
 	if err == nil {
 		database = client.Database(name)
+		media = database.Collection("media")
 	}
 
 	return
+}
+
+func DropDB(name string) {
+	client.Database(name).Drop(context.TODO())
+}
+
+func NewUUID() (id string, err error) {
+	var exists bool
+
+	for true {
+		id = uuid.NewV4().String()
+		_, exists, err = GetUnique(bson.D{{"id", id}})
+
+		if err != nil || !exists {
+			break
+		}
+	}
+
+	return
+}
+
+func GetUnique(filt bson.D) (result bson.M, exists bool, err error) {
+	exists = false
+
+	var cursor *mongo.Cursor
+	cursor, err = media.Find(timeout_ctx(5), filt)
+
+	if err != nil {
+		return
+	}
+
+	var results []bson.M
+	err = cursor.All(timeout_ctx(5), &results)
+
+	if len(results) > 1 {
+		err = util.TooManyResults(len(results))
+	}
+
+	if len(results) != 0 {
+		exists = true
+		result = results[0]
+	}
+
+	return
+}
+
+func CreateReference(id, mime string) {
+	var writable map[string]interface{} = map[string]interface{}{
+		"id":      id,
+		"mime":    mime,
+		"created": time.Now().Unix(),
+	}
+
+	media.InsertOne(context.Background(), writable)
 }
---
+++ storage/storage_test.go
@@ -0,0 +1,89 @@
+package storage
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"os"
+	"testing"
+)
+
+var id string
+
+func TestMain(main *testing.M) {
+	ConnectTo("foobar:foobar2000", "localhost", "test_storage")
+	DropDB("test_storage")
+
+	id, _ = NewUUID()
+	CreateReference(id, "image/jpg")
+
+	os.Exit(main.Run())
+	DropDB("test_storage")
+}
+
+func Test_GetUnique(test *testing.T) {
+	var result bson.M
+	var exists bool
+	var err error
+	result, exists, err = GetUnique(bson.D{{"id", id}})
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	if !exists {
+		test.Errorf("Test_GetUnique: UUIDv4 %s does not exist", id)
+		test.FailNow()
+	}
+
+	var retrieved string = result["id"].(string)
+	if retrieved != id {
+		test.Errorf("Test_GetUnique: UUIDv4 mismatch: have %s, want %s", retrieved, id)
+	}
+}
+
+func Test_GetUnique_dupe(test *testing.T) {
+	var dupe_id string = "foobar"
+	var writable map[string]interface{} = map[string]interface{}{
+		"id": dupe_id,
+	}
+
+	media.InsertOne(context.Background(), writable)
+	media.InsertOne(context.Background(), writable)
+
+	var exists bool
+	var err error
+	_, exists, err = GetUnique(bson.D{{"id", dupe_id}})
+
+	if !exists {
+		test.Errorf("Test_GetUnique_dupe: dupe key %s does not exist", dupe_id)
+	}
+
+	if err == nil {
+		test.Error("Test_GetUnique_dupe: no err returned")
+	}
+
+}
+
+func Test_NewUUID(test *testing.T) {
+	var new_id string
+	var err error
+	var exists bool
+	var count int = 1000
+
+	for count != 0 {
+		count -= 1
+		new_id, err = NewUUID()
+
+		_, exists, err = GetUnique(bson.D{{"id", new_id}})
+
+		if err != nil {
+			test.Fatal(err)
+		}
+
+		if exists {
+			test.Errorf("Test_NewUUID: UUID %s already exists", new_id)
+		}
+
+		media.InsertOne(context.Background(), bson.D{{"id", new_id}})
+	}
+}
