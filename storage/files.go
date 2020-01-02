a44d765e1272753aec5a6813fcf34ceaed318aa0
Noah Harris <jnharris@boomtownroi.com>
2020-01-02 00:34:15 -0500

    add: Inteface to write files to disk
    Add an interface to write files to the disk when uploading

---
+++ storage/files.go
@@ -0,0 +1,27 @@
+package storage
+
+import (
+	"io"
+	"os"
+)
+
+var file_root string
+
+func SetFileRoot(where string) (err error) {
+	file_root = where
+	err = os.MkdirAll(where, os.ModePerm)
+	return
+}
+
+func WriteMultipartFile(id string, file io.Reader) (path string, err error) {
+	var out *os.File
+	defer out.Close()
+
+	out, err = os.Create(file_root + id)
+	if err != nil {
+		return
+	}
+
+	io.Copy(out, file)
+	return
+}
