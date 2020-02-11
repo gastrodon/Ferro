bfaeccb041cc62267e9b669acfd472cb85203f3d
Noah Harris <jnharris@boomtownroi.com>
2020-02-11 09:08:05 -0500

    Add port flag

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	var file_root *string = flag.String("at", "/monke/files/", "File storage root")
 	var csr *string = flag.String("csr", "", "csr to use for SSL")
 	var key *string = flag.String("key", "", "key to use for SSL")
-	var port *int = flag.Int("p", 8000, "port to serve")
+	var port *int = flag.Int("port", 8000, "port to serve")
 	flag.Parse()
 
 	var err error = storage.ConnectTo(fmt.Sprintf("%s:%s", mongo_uname, mongo_pass), mongo_host, "monke-cdn")
